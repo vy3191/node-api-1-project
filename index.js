62aaeb9a66963809a8a15dec8ae0697383a3e3ac
vy3191 <venkyafhs@gmail.com>
2020-02-12 19:30:53 -0500

    initial project set up done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require('express');
+const database = require("./data/db");
+const server = express();
+const PORT = 8080;
+
+server.get('/api/users', (req,res) => {
+   
+});
+server.get('/api/users/:id', (req,res) => {
+   const id = req.params.id;
+
+   
+});
+server.post('/api/users', (req,res) => {
+   
+   
+});
+server.put('/api/users/:id', (req,res) => {
+   const id = req.params.id;
+   
+});
+server.delete('/api/users', (req,res) => {
+   
+});
+
+
+server.listen(PORT, () => {
+   console.log(`Server is running at https://localhost${PORT}`);
+})
