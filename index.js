20f84e68f401c21b0a45b273f48a4e99037c6a55
vy3191 <venkyafhs@gmail.com>
2020-02-12 19:36:25 -0500

    Done wit get request

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // implement your API here
 const express = require('express');
-const database = require("./data/db");
+const db = require("./data/db");
 const server = express();
 const PORT = 8080;
 
 server.get('/api/users', (req,res) => {
-   
+   db.find()
+           .then( response => {
+              if(response) {
+                 res.status(200).json(response)
+              } else {
+                 res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+              }
+           })
+           .catch(err => {
+              console.log(err);
+           })
 });
 server.get('/api/users/:id', (req,res) => {
    const id = req.params.id;
